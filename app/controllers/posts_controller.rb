55eaac56d1810420224eebe8e0e72345c6c9786e
lucidus21 <lucidus21@gmail.com>
2014-01-25 17:11:05 +0900

    show action 만들어보기

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,14 +2,14 @@ class PostsController < ApplicationController
   def new
   end
   def create
-    puts "debug==========="
-    p params
-    puts "debug==========="
     @post = Post.new
     @post.title = params[:title]
     @post.body = params[:body]
     @post.save
 
     redirect_to "/posts/new"
+  end
+  def show
+    @post = Post.find(params[:id])
   end
 end
