466519b4457d538b83d17d6144512a70c05cdc25
lucidus21 <lucidus21@gmail.com>
2014-01-25 16:37:28 +0900

    posts#create 액션 완성

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,16 @@ class PostsController < ApplicationController
   def new
   end
   def create
-    render text: "#{params.inspect}"
+    puts "debug==========="
+    p params
+    puts "debug==========="
+    @post = Post.new
+    @post.title = params[:title]
+    @post.body = params[:body]
+    @post.save
+
+    # .attributes는 instance의 모든 attribtue들의 상태를 확인할 수 있습니다.
+    # @post의 id/ created_at / updated_at attribute들은 rails가 생성해줍니다.
+    render text: "#{@post.attributes}"
   end
 end
