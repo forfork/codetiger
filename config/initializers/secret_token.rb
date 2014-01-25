534c93efd22fede3c9fc6d378309685ceaf8dba1
lucidus21 <lucidus21@gmail.com>
2014-01-25 15:56:25 +0900

    rails g controller welcome index까지

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Codetiger::Application.config.secret_key_base = '3588161fe0d911ba4b7dd22353a294b1aea434feeef02b271910c16be6df679c4f857e5712ba4461d7eac3dbab7a68230a5b88e8c59f0223770a7c09b0fbce19'
