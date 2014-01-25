3e5db881da191d9b9e0e134f6ae482379960a3f1
lucidus21 <lucidus21@gmail.com>
2014-01-25 16:43:05 +0900

    create action redirect to new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,8 +10,6 @@ class PostsController < ApplicationController
     @post.body = params[:body]
     @post.save
 
-    # .attributes는 instance의 모든 attribtue들의 상태를 확인할 수 있습니다.
-    # @post의 id/ created_at / updated_at attribute들은 rails가 생성해줍니다.
-    render text: "#{@post.attributes}"
+    redirect_to "/posts/new"
   end
 end
