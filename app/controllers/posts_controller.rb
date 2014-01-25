752370eb97220b42be9cef1cebceec4c14e16f15
lucidus21 <lucidus21@gmail.com>
2014-01-25 18:14:13 +0900

    destory 구현을 마지막으로 CRUD 종료, Readme 까지 같이 수정

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -37,4 +37,10 @@ class PostsController < ApplicationController
       render :edit
     end
   end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to "/posts"
+  end
 end
