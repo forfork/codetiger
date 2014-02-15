76e2e690ddb55178a926482b0e6636b35c8f10e6
lucidus21 <lucidus21@gmail.com>
2014-02-15 21:31:41 +0900

    user table 생성

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140207171452) do
+ActiveRecord::Schema.define(version: 20140215123034) do
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20140207171452) do
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
