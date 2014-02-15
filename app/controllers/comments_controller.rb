c2484b6d2a676958bc602c6f597232ab1f6c4d92
lucidus21 <lucidus21@gmail.com>
2014-02-15 21:26:48 +0900

    ajax 종료

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommentsController < ApplicationController
+  def create
+    comment = Comment.new(commenter: params[:ct],
+                          body: params[:bd],
+                          post_id: params[:post_id])
+    if comment.save
+      puts "########################"
+      puts comment.attributes
+      puts "########################"
+    end
+
+    data = {
+      ctt: comment.commenter,
+      bdd: comment.body
+    }
+
+    render json: data.to_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Codetiger::Application.routes.draw do
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  
+  resource :comment
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
